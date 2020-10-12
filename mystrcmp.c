4a3d963941b774297caa00b8f5cd2cc27893a258
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-12 22:12:49 +0530

    cmp funcn created

---
+++ mystrcmp.c
@@ -0,0 +1,16 @@
+void mystrcmp(char str1[] ,char str2[])
+{
+   printf("Enter two strings : \n");
+   gets(str1);
+   gets(str2);
+
+   int count = 0;
+   while (str1[count] == str2[count] && str1[count] != '\0')
+      count++;
+   if (str1[count] > str2[count])
+      printf("str1 is greater than str2");
+   else if (str1[count] < str2[count])
+      printf("str1 is less than str2");
+   else
+      printf("str1 is equal to str2");
+}
