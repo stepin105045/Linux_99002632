094fd19a6e3250818f7a14622b12e146da532206
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-12 21:30:51 +0530

    mystrcpy made

---
+++ mystrcpy.c
@@ -0,0 +1,16 @@
+
+
+int mystrcpy(char str1,char str2)
+{
+
+    printf("enter str1: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    for (ind = 0; str1[ind] != '\0'; ++ind)
+    {
+            str2[ind] = str1[ind];
+    }
+    str2[i] = '\0';
+    printf("str2: %s", str2);
+    return 0;
+}
