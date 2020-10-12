2a64b67c958e0c876ea35cd7cac0e37a60714947
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-12 22:28:13 +0530

    fact funcn created

---
+++ factorial.c
@@ -0,0 +1,13 @@
+long factorial(int num)
+{
+   int ind;
+   long int result=1;
+   if(num==0)
+   return 1;
+   else
+   {
+    for( ind = 1 ; ind <= num ; ind++ )
+        result = result*ind;
+    return result;
+   }
+}
