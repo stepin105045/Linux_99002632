6a6ca92efbd38951f30ea080cb659301bf3a104e
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-12 22:46:21 +0530

    palindrome fun

---
+++ src/isPalindrome.c
@@ -0,0 +1,15 @@
+int isPalindrome(int num)
+{
+    int rnum=0,rem,cp;
+    cp=num;
+    while(cp>0)
+    {
+        rem=cp%10;
+        rnum=rnum*10+rem;
+        cp=cp/10;
+    }
+    if(rnum==num)
+        return 1;
+    else
+        return 0;
+}
