229e9402157b5534582468803520905d780d03aa
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-12 22:59:00 +0530

    Header 1 created

---
+++ include/mystring.h
@@ -0,0 +1,13 @@
+#ifndef __MYSTRING_H__
+#define __MYSTRING_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void mystrcat(char str1[], char str2[]);
+void mystrcmp(char str1[] ,char str2[]);
+void mystrcpy(char str1[] ,char str2[]);
+int mystrlen(char str[]);
+
+#endif
