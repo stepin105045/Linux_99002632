6968ba8eb86d19b65d8de8ce450bbe029495263d
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 09:05:21 +0530

    vsum created

---
+++ src/vsum.c
@@ -0,0 +1,16 @@
+int vsum(int number,...)
+{
+    int ind;
+    int first, a;
+    
+    va_list vlist;
+    va_start(vlist,number);
+  
+    int sum=0;
+    
+    for (ind = 1; ind <= number; ind++) 
+        sum+=va_arg(vlist,int);
+
+    va_end(vlist);
+    return sum;
+}
