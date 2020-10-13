147e8d1a57cc4dc591b93592937d84fcbea6b51c
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 09:52:12 +0530

    test file created 1

--- test1.c
+++ test1.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
-#include "basic.h"
+#include "mystring.h"
+
 int main()
 {
-    char aj1[100], aj2[100];
+    char s1[100], s2[100];
     int compare;
 
     printf("\n\nEnter 1st string: ");
-    scanf("%s", aj1);
+    scanf("%s", s1);
 
     printf("\n\nEnter 2nd string: ");
-    scanf("%s", aj2);
+    scanf("%s", s2);
 
-    compare = mystrcmp(aj1, aj2);  // function call
+    compare = mystrcmp(s1, s2);  // function call
 
     if(compare == 1)
         printf("\n\nBoth the strings are exactly same.\n\n");
      else
-        printf("\n\nBoth the strings are different.\n");
-    printf("the length : %d" ,mystrlen(aj1));
-    mystrcpy(aj1);
-    mystrcat(aj1,aj2);
+        printf("\n Both the strings are different \n");
+    
+    printf("the length : %d" ,mystrlen(s1));
+    mystrcpy(s1,s2);
+    mystrcat(s1,s2);
+    
     return 0;
 }
