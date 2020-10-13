ef3c82451f262a4c47ac9058bfde368df573cbe3
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 11:32:37 +0530

    Update test3.c

--- test3.c
+++ test3.c
@@ -1,12 +1,11 @@
-
 #include "bitmask.h"
 int main()
 {
     int n = 5, k = 3;
 
-    printf("%d with %d-th bit set: %d\n", n, k, set(n, k));
-    printf("%d with %d-th bit reset: %d\n", n, k, reset(n, k));
-    printf("%d with %d-th bit flipped: %d\n", n, k, flip(n, k));
+    printf("%d with %d-th bit set: %d \n", n, k, set(n, k));
+    printf("%d with %d-th bit reset: %d \n", n, k, reset(n, k));
+    printf("%d with %d-th bit flipped: %d \n", n, k, flip(n, k));
     query(5,2);
     return 0;
 }
