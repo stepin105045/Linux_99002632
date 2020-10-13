6e7e2418b70d1380a00306683c58c35aece1fcc8
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 08:47:15 +0530

    Update mystrlen.c

--- src/mystrlen.c
+++ src/mystrlen.c
@@ -1,7 +1,7 @@
 int mystrlen(char str[])
 {
-    printf("Enter a string: \n");
-    scanf("%s",str);
+    //printf("Enter a string: \n");
+    //scanf("%s",str);
     int len;
     for(len=0; str[len]!='\0'; ++len);
     printf("\n Length of the given string: %d",len);
