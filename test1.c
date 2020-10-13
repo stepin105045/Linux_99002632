7f348fd7071b829630ece78ddceaa4ff8303d94f
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 11:31:49 +0530

    Update test1.c

--- test1.c
+++ test1.c
@@ -12,14 +12,8 @@ int main()
     printf("\n\nEnter 2nd string: ");
     scanf("%s", s2);
 
-    compare = mystrcmp(s1, s2);  // function call
-
-    if(compare == 1)
-        printf("\n\nBoth the strings are exactly same.\n\n");
-     else
-        printf("\n Both the strings are different \n");
-    
-    printf("the length : %d" ,mystrlen(s1));
+    mystrcmp(s1, s2);  // function call
+    mystrlen(s1);
     mystrcpy(s1,s2);
     mystrcat(s1,s2);
     
