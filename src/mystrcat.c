a964e6a242db84698bd757b30b04a80506e47f92
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 08:46:03 +0530

    Update mystrcat.c

--- src/mystrcat.c
+++ src/mystrcat.c
@@ -1,9 +1,9 @@
 void mystrcat(char str1[], char str2[])
 {
-    printf("enter str1: ");
-    fgets(str1, sizeof(str1), stdin);
-    printf("enter str2: ");
-    fgets(str2, sizeof(str2), stdin);
+   // printf("enter str1: ");
+    //fgets(str1, sizeof(str1), stdin);
+    //printf("enter str2: ");
+//fgets(str2, sizeof(str2), stdin);
     int length = 0;
     int j;
     while (str1[length] != '\0')
