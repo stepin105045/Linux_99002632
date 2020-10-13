c4846e276b9deda3b348bc75a112460c50286984
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 10:03:13 +0530

    Update myutils.h

--- include/myutils.h
+++ include/myutils.h
@@ -3,6 +3,7 @@
 #define __MYUTILS_H__
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <stdarg.h>
 
@@ -12,3 +13,18 @@ int isPrime(int n);
 int vsum(int number,...);
 
 #endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "fun.h"
+int main()
+{
+    int num;
+    printf("Enter the Number");
+    scanf("%d",&num);
+    printf("\nPrime:%d",isPrime(num));
+    printf("\nFactorial:%d",factorial(num));
+    printf("\nPalindrome:%d",isPalindrome(num));
+    printf("\nVsum:%d",vsum(3,3,4,5));
+    return 0;
+}
