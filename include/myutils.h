da9e2f052e779e6f8c80752ab0f649963e50bb07
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 10:03:41 +0530

    Update myutils.h

--- include/myutils.h
+++ include/myutils.h
@@ -13,18 +13,4 @@ int isPrime(int n);
 int vsum(int number,...);
 
 #endif
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include "fun.h"
-int main()
-{
-    int num;
-    printf("Enter the Number");
-    scanf("%d",&num);
-    printf("\nPrime:%d",isPrime(num));
-    printf("\nFactorial:%d",factorial(num));
-    printf("\nPalindrome:%d",isPalindrome(num));
-    printf("\nVsum:%d",vsum(3,3,4,5));
-    return 0;
-}
+
