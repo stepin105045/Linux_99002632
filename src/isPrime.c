bb0991f5f2e0f0e8dbc61641d33d5bcb0cd932c2
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 11:30:11 +0530

    Update isPrime.c

--- src/isPrime.c
+++ src/isPrime.c
@@ -1,11 +1,13 @@
+#include"myutils.h"
 int isPrime(int n)
 {
 	int prime;
-	for(prime=2;prime <=n/2;i++)
+	for(prime=2;prime <=n/2;prime++)
 	{
 		if(n%prime!=0)
 			continue; // prime number
 		else
 			return 1;
+		return 2;
 	}
 }
