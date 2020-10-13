a8caad5520acfe89f10ab65134e89945c3549d82
99002632 <71488754+99002632@users.noreply.github.com>
2020-10-13 08:46:43 +0530

    Update mystrcmp.c

--- src/mystrcmp.c
+++ src/mystrcmp.c
@@ -1,8 +1,8 @@
 void mystrcmp(char str1[] ,char str2[])
 {
-   printf("Enter two strings : \n");
-   gets(str1);
-   gets(str2);
+   //printf("Enter two strings : \n");
+   //gets(str1);
+   //gets(str2);
 
    int count = 0;
    while (str1[count] == str2[count] && str1[count] != '\0')
